f87658a5fad7e1a0959ed84d4e99774174d16745
Wyatt Calandro <wyatt@wcalandro.com>
2018-06-19 09:36:40 -0700

    Remove mux and add chi

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"net/http"
+	"time"
 
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
 	// Initialize the main router
-	r := mux.NewRouter()
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	http.ListenAndServe(":8000", r)
 }
