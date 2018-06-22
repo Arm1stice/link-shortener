1157b4ef4d2ed0fb63f47325a92d69d76e232cf9
Wyatt Calandro <wyatt@wcalandro.com>
2018-06-22 10:19:02 -0700

    Add new dependencies

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/volatiletech/authboss"
+	_ "github.com/volatiletech/authboss/auth"
+	_ "github.com/volatiletech/authboss/confirm"
+	_ "github.com/volatiletech/authboss/lock"
+	_ "github.com/volatiletech/authboss/recover"
+	_ "github.com/volatiletech/authboss/register"
+	_ "github.com/volatiletech/authboss/remember"
 )
+
+var ab = authboss.New()
 
 func main() {
 	// Initialize the main router
@@ -18,5 +28,13 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello")
+	})
 	http.ListenAndServe(":8000", r)
+}
+
+func authbossSetup() {
+	ab := authboss.New()
+
 }
